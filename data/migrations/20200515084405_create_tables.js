e18cc7bb4ed2087869abb25115bc80e568fb0812
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-15 09:14:31 -0700

    table for project_resources is up and running! migrations file completed

--- data/migrations/20200515084405_create_tables.js
+++ data/migrations/20200515084405_create_tables.js
@@ -1,39 +1,67 @@
 
-exports.up = function(knex) {
-  return knex.schema
-  .createTable('projects', tbl =>{
-      tbl.increments();
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments();
 
-      tbl.string('projectName')
-      .unique()
-      .notNullable();
+            tbl.string('projectName')
+                .unique()
+                .notNullable();
 
-      tbl.string('description', 255);
+            tbl.string('description', 255);
 
-      tbl.boolean('completed').default(false);
-  })
-  .createTable('tasks', tbl =>{
-      tbl.increments();
+            tbl.boolean('completed').default(false);
+        })
 
-      tbl.string('description', 255).notNullable();
+        .createTable('tasks', tbl => {
+            tbl.increments();
 
-      tbl.string('notes', 175);
+            tbl.string('description', 255).notNullable();
 
-      tbl.boolean('completed').default(false);
+            tbl.string('notes', 175);
 
-      tbl.integer('project_id')
-      .unsigned()
-      .notNullable()
-      .references('projects.id')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
+            tbl.boolean('completed').default(false);
 
-  })
-  
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('projects.id')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+
+        })
+
+        .createTable('resources', tbl => {
+            tbl.increments()
+
+            tbl.string('resourceName', 155).notNullable();
+
+            tbl.string('description', 255);
+        })
+
+        .createTable('project_resources', project_resourcea => {
+            project_resourcea.increments();
+
+            project_resourcea.string('quantity');
+
+            project_resourcea.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project.id')
+
+            project_resourcea.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resource.id')
+
+        })
+
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema
-    .dropTableIfExists('tasks')
-    .dropTableIfExists('project')
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('projects')
 };
