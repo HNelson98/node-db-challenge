fb7c6f47bfe9c8e1ee63e5c07aa1770a5400232a
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-15 09:03:32 -0700

    tasks table is up and running

--- data/migrations/20200515084405_create_tables.js
+++ data/migrations/20200515084405_create_tables.js
@@ -1,20 +1,39 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable('project', tbl =>{
+  .createTable('projects', tbl =>{
       tbl.increments();
 
       tbl.string('projectName')
       .unique()
       .notNullable();
 
-      tbl.string('description', 255)
+      tbl.string('description', 255);
 
-      tbl.boolean('completed').default(false)
+      tbl.boolean('completed').default(false);
   })
+  .createTable('tasks', tbl =>{
+      tbl.increments();
+
+      tbl.string('description', 255).notNullable();
+
+      tbl.string('notes', 175);
+
+      tbl.boolean('completed').default(false);
+
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('projects.id')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+  })
+  
 };
 
 exports.down = function(knex) {
     return knex.schema
+    .dropTableIfExists('tasks')
     .dropTableIfExists('project')
 };
