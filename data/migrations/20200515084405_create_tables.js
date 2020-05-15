34cb132cfffc28e44ac4e9d0bc45db8187e40073
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-15 08:53:17 -0700

    projects table is set up

--- data/migrations/20200515084405_create_tables.js
+++ data/migrations/20200515084405_create_tables.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('project', tbl =>{
+      tbl.increments();
+
+      tbl.string('projectName')
+      .unique()
+      .notNullable();
+
+      tbl.string('description', 255)
+
+      tbl.boolean('completed').default(false)
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('project')
 };
