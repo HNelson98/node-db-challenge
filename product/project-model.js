9d72db085bcd790916108251b77cedd16051b4ae
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-15 10:47:17 -0700

    got everything but the get tasks wokring annd done

--- product/project-model.js
+++ product/project-model.js
@@ -17,18 +17,29 @@ function getResources() {
     return db('resources');
 }
 
-function getTasks() {
+function getTasks(id) {
+    return db('tasks')
+    .join('projects', 'projects.id', '=', 'tasks.project_id')
+    .select('project.projectName', 'project.description', 'task.description', 'task.notes', 'task.completed')
+    .where('projects.id', id)
 
+
+    // return db('tasks').where('projects_id', id)
+    // .join('projects', 'tasks.projects_id', 'projects.id')
+    // .select('project.projectName', 'project.description', 'task.description', 'task.notes', 'task.completed')
+    
 }
 
-function addProject(){
-
+function addProject(newProject){
+    return db('Projects')
+    .insert(newProject)
 }
 
 function addTask() {
 
 }
 
-function addResource() {
-
+function addResource(newResourcse) {
+    return db('resources')
+    .insert(newResourcse)
 }
--- product/projects-router.js
+++ product/projects-router.js
@@ -7,22 +7,62 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     Projects.getProjects()
-    .then(projects => {
-        res.status(200).json(projects)
-    })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get projects' });
-    });
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get projects' });
+        });
 })
 
 router.get('/resources', (req, res) => {
     Projects.getResources()
-    .then(resources => {
-        res.status(200).json(resources)
-    })
-    .catch(err => {
-        res.status(500).json({ message: 'Failed to get resources' });
-    });
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get resources' });
+        });
 })
 
+router.get('/:id/tasks', (req, res) => {
+    const { id } = req.params;
+
+    Projects.getTasks(id)
+        .then(tasks => {
+            if (tasks.length) {
+                res.json(tasks);
+            } else {
+                res.status(404).json({ message: 'Could not find tasks for given project' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get tasks' });
+        });
+});
+
+
+router.post('/', (req, res) => {
+    const newProject = req.body;
+
+    Projects.addProject(newProject)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to create new project' });
+        });
+});
+
+router.post('/resources', (req, res) => {
+    const newResource = req.body;
+
+    Projects.addResource(newResource)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to create new resource' });
+        });
+});
 module.exports = router;
