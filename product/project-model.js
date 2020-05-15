117722f934c0c4db7550cb04b3c90b20a39fd4fb
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-15 10:01:32 -0700

    server and router up and running

---
+++ product/project-model.js
@@ -0,0 +1,33 @@
+const db = require('../data/db-config')
+
+module.exports = {
+    getResources,
+    getProjects,
+    getTasks,
+    addResource,
+    addProject,
+    addTask
+}
+
+function getProjects() {
+
+}
+
+function getResources() {
+}
+
+function getTasks() {
+
+}
+
+function addProject(){
+
+}
+
+function addTask() {
+
+}
+
+function addResource() {
+    
+}
